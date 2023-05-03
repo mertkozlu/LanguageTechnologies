57da487b56d90369bf57fc10880c01f368de24b4
mertkozlu <kozlumert@outlook.com>
2023-05-03 18:29:53 +0300

    k

--- src/main/java/language/technologies/business/concretes/SubTechnologyManager.java
+++ src/main/java/language/technologies/business/concretes/SubTechnologyManager.java
@@ -1,20 +1,25 @@
 package language.technologies.business.concretes;
 
 import language.technologies.business.abstracts.SubTechnologyService;
+import language.technologies.dataAccess.abstracts.ProgrammingLanguageRepository;
 import language.technologies.dto.requests.CreateSubTechnologiesRequest;
 import language.technologies.dto.responses.GetAllSubTechnologyResponse;
 import language.technologies.dataAccess.abstracts.SubTechnologyRepository;
+import language.technologies.entities.ProgrammingLanguage;
 import language.technologies.entities.SubTechnology;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Service
 public class SubTechnologyManager implements SubTechnologyService {
     private SubTechnologyRepository subTechnologyRepository;
+    private ProgrammingLanguageRepository programmingLanguageRepository;
 
-    public SubTechnologyManager(SubTechnologyRepository subTechnologyRepository) {
+    public SubTechnologyManager(SubTechnologyRepository subTechnologyRepository, ProgrammingLanguageRepository programmingLanguageRepository) {
         this.subTechnologyRepository = subTechnologyRepository;
+        this.programmingLanguageRepository = programmingLanguageRepository;
     }
 
     @Override
@@ -34,7 +39,10 @@ public class SubTechnologyManager implements SubTechnologyService {
     @Override
     public void add(CreateSubTechnologiesRequest createSubTechnologiesRequest) {
         SubTechnology subTechnology = new SubTechnology();
+        ProgrammingLanguage programmingLanguage = programmingLanguageRepository.findById(createSubTechnologiesRequest.getLanguageId()).get();
+
         subTechnology.setName(createSubTechnologiesRequest.getName());
+        subTechnology.setProgrammingLanguage(programmingLanguage);
         this.subTechnologyRepository.save(subTechnology);
 
     }
